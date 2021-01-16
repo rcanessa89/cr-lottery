5cff5477e4a3646d372800656974e1c7efb4b7de
rcanessa89 <rcanessa89@hotmail.com>
2021-01-15 22:10:51 -0600

    precommit dependencies installed

--- libs/utils/src/lib/pluralize.ts
+++ libs/utils/src/lib/pluralize.ts
@@ -1,7 +1,6 @@
 export const pluralize = (str: string) => {
   const strTrimed = str.trim();
   const lastLetter = strTrimed[strTrimed.length - 1];
-  const plural = lastLetter === 'y' ? 'ies' : 's';
 
   if (lastLetter === 'y') {
     return strTrimed.substring(0, strTrimed.length - 1) + 'ies';
